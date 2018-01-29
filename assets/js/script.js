f94d6533bd3eebd781c615d2d7e01fb2404f7a2d
Denis <denis.dimitriy@gmail.com>
2018-01-30 00:11:19 +0200

    add scrolled progressbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,14 +12,38 @@ $(function () {
         }
     });
     $('#toTop').click(function () {
-        $('body,html').animate({scrollTop: 0}, 800);
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
     });
 
     /** Smoothly scroll */
-    $('a[href^="#"]').click(function(){
+    $('a[href^="#"]').click(function () {
         var target = $(this).attr('href');
-        $('html, body').animate({scrollTop: $(target).offset().top}, 600);
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 600);
         return false;
+    });
+
+    $(window).scroll(function () {
+        if ($(window).width() <= 768) {
+            if ($(window).scrollTop() > 1600) {
+                $(".skills .progress-bar").addClass("scrolled")
+            }
+        } else if ($(window).width() > 768 && $(window).width() <= 992) {
+            if ($(window).scrollTop() > 900) {
+                $(".skills .progress-bar").addClass("scrolled")
+            }
+        } else if ($(window).width() > 992 && $(window).width() <= 1200) {
+            if ($(window).scrollTop() > 850) {
+                $(".skills .progress-bar").addClass("scrolled")
+            }
+        } else {
+            if ($(window).scrollTop() > 850) {
+                $(".skills .progress-bar").addClass("scrolled")
+            }
+        }
     });
 
 });
